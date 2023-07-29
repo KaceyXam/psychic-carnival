dce3cf368a8ad42ab9c45f11201e3ae03a6434db
kaceykniffen <kaceykniffen@gmail.com>
2023-07-28 21:35:53 -0500

    Read markdown file

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	mdfile, err := os.ReadFile("test-files/hello-world.md")
+	if err != nil {
+		panic(err)
+	}
+
+	parsedmd := ParseMarkdown(mdfile)
+
+	for _, item := range parsedmd {
+		fmt.Println(item)
+	}
+
+}
+
+func ParseMarkdown(md []byte) []string {
+	res := strings.Split(WindowsSucks(string(md)), "\n")
+	return DeleteEmpty(res)
+}
+
+func WindowsSucks(file string) string {
+	return strings.ReplaceAll(string(file), "\r\n", "\n")
+}
+
+func DeleteEmpty(s []string) []string {
+	var r []string
+	for _, str := range s {
+		if str != "" {
+			r = append(r, str)
+		}
+	}
+	return r
 }
